056e66c1acc2b68b080f38178e2f179b00e5f53e
Niken <niken@Nikens-MacBook-Pro.local>
2023-03-27 08:11:40 +0545

    FIRST COMMIT

---
+++ lib/FaceDetectorView.dart
@@ -0,0 +1,87 @@
+
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:camera/camera.dart';
+import 'package:face_recognition/CameraView.dart';
+import 'package:face_recognition/FaceDetectorPainter.dart';
+import 'package:face_recognition/facenet_service.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'package:tflite_flutter/tflite_flutter.dart'as tflite;
+import 'package:image/image.dart'as imglib;
+
+class FaceDetectorView extends StatefulWidget {
+  @override
+  State<FaceDetectorView> createState() => _FaceDetectorViewState();
+}
+
+class _FaceDetectorViewState extends State<FaceDetectorView> {
+  final FaceNetService _faceNetService = FaceNetService.faceNetService;
+
+  final FaceDetector _faceDetector = FaceDetector(
+    options: FaceDetectorOptions(
+      enableContours: true,
+      enableClassification: true,
+    ),
+  );
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+  String? _userName;
+
+  @override
+  void dispose() {
+    _canProcess = false;
+    _faceDetector.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CameraView(
+      title: 'Face Detector',
+      customPaint: _customPaint,
+      text: _text,
+      onImage: (inputImage) {
+        processImage(inputImage);
+      },
+      onUserName:(userName) {
+          _userName = userName;
+      },
+      initialDirection: CameraLensDirection.front,
+    );
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    final faces = await _faceDetector.processImage(inputImage);
+    if (inputImage.inputImageData?.size != null &&
+        inputImage.inputImageData?.imageRotation != null) {
+      final painter = FaceDetectorPainter(
+          faces,
+          inputImage.inputImageData!.size,
+          inputImage.inputImageData!.imageRotation);
+      _customPaint = CustomPaint(painter: painter);
+    } else {
+      String text = 'Faces found: ${faces.length}\n\n';
+      for (final face in faces) {
+        text += 'face: ${face.boundingBox}\n\n';
+        // _faceNetService.setCurrentPrediction(cameraImage, face);
+      }
+      _text = text;
+      // TODO: set _customPaint to draw boundingRect on top of image
+      _customPaint = null;
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+}
